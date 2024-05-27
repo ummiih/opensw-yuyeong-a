34c84f87296ea5622e73bbb5058a34c336fb24ed
Parksunyoung1 <parksunyoung1012@gmail.com>
2024-05-27 11:18:46 +0900

    로그인 시작 화면 추가

---
+++ seoro-front/src/app/(main)/login1/page.tsx
@@ -0,0 +1,18 @@
+
+const Login1 = () => {
+    return (
+        <>
+            <div className={'bg-[#DCDCF4] min-h-screen'}>
+              <div className={'fixed bottom-0 flex flex-col gap-y-[5px] px-[20px]'}>
+                <div className={'px-[120px] py-[15px]  w-[335px] h-[52px] bg-[#8D8DC1] rounded-[10px] '}>
+                  <div className={'text-white text-[14px]'}>서로 시작하기</div>
+                </div>
+                <div className={'px-[100px] py-[8px]'}>
+                  <hr className="w-[134px] rounded-[100px] border-[3.5px] border-black" />
+                </div>
+              </div>
+            </div>
+        </>
+    )
+}
+export default Login1;
