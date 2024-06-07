0bcc7cf0482c21d4d8469fd4ef8648995e0ec654
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-06-07 10:25:52 +0900

    feat: filter 퍼블리싱 구현

--- seoro-front/src/app/(main)/magazine/page.tsx
+++ seoro-front/src/app/(main)/magazine/page.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import Header from "@/components/magazine/Header";
 import ArticlePost from "@/components/magazine/ArticlePost";
 import Navbar from "@/components/Navbar";
 import * as React from "react";
-import type { SVGProps } from "react";
+import {SVGProps, useState} from "react";
+import {FilterType} from "@/types/magazine/type";
+import Filter from "@/components/magazine/Filter";
+import {filterContents} from "@/utils/magazine/filterContents";
 
 const Magazine = () => {
+    const [isFilterClicked, setIsFilterClicked] = useState<boolean>(false);
+    const [filterContent, setFilterContent] = useState<FilterType>('최신순');
     return (
-        <div className={'bg-[#DCDCF4] min-h-screen flex flex-col gap-y-4'}>
+        <div className={'bg-[#DFDFED] min-h-screen flex flex-col gap-y-4'}>
             <Header />
-            <div className={'mx-5 flex items-center gap-x-2 bg-white rounded-full w-fit px-3 py-[5px]'}>
-                <div className={'text-[#727375] '}>최신순</div>
-                <Icon></Icon>
+            {/* 필터 */}
+            <div
+                onClick={() => {
+                    setIsFilterClicked(!isFilterClicked);
+                }}
+                className={'relative mx-5 flex items-center gap-x-2 bg-white rounded-full w-fit px-3 py-[5px]'}>
+                <div className={'text-[#727375] text-[14px]'}>{filterContent}</div>
+                <Icon />
             </div>
+            { isFilterClicked ? <Filter filterContents={filterContents} setFilterContent={setFilterContent} setIsFilterClicked={setIsFilterClicked}/> : null }
             <div className={'px-5 flex flex-col gap-y-4'}>
                 <ArticlePost />
                 <ArticlePost />
